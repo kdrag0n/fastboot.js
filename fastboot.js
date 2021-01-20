3cc12b68532fc13640875821756f6381c28d4cf0
Danny Lin <danny@kdrag0n.dev>
2021-01-19 22:17:20 -0800

    Implement basic sparse image flashing support
    Tested with a <512M sparse image on a Pixel 2 XL, which reports a max
    download size of 512 MiB. Device successfully booted to setup with the
    vendor image flashed with this client.

--- fastboot.js
+++ fastboot.js
@@ -2,12 +2,28 @@ const FASTBOOT_USB_CLASS = 0xff;
 const FASTBOOT_USB_SUBCLASS = 0x42;
 const FASTBOOT_USB_PROTOCOL = 0x03;
 
+const DEFAULT_DOWNLOAD_SIZE = 512 * 1024 * 1024; // 512 MiB
+
 const DEBUG = true;
 
 function logDebug(...data) {
     if (DEBUG) {
         console.log(...data);
     }
+}
+
+function readFileAsBuffer(file) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.onerror = () => {
+            reject(reader.error);
+        };
+
+        reader.readAsArrayBuffer(file);
+    });
 }
 
 export class UsbError extends Error {
@@ -29,6 +45,7 @@ export class FastbootError extends Error {
 export class FastbootDevice {
     constructor() {
         this.device = null;
+        this.maxPacketSize = null;
     }
 
     async connect() {
@@ -68,6 +85,8 @@ export class FastbootDevice {
             } else if (endpoint.direction == 'out') {
                 if (epOut == null) {
                     epOut = endpoint.endpointNumber;
+                    // Device reports max packet size according to spec
+                    this.maxPacketSize = endpoint.packetSize;
                 } else {
                     throw new UsbError('Interface has multiple OUT endpoints');
                 }
@@ -82,6 +101,36 @@ export class FastbootDevice {
         await this.device.claimInterface(0); // fastboot
     }
 
+    async readResponse() {
+        let returnData = {
+            text: '',
+            dataSize: null,
+        };
+        let response;
+        do {
+            let respPacket = await this.device.transferIn(0x01, 64);
+            response = new TextDecoder().decode(respPacket.data);
+            logDebug('response: packet', respPacket, 'string', response);
+    
+            if (response.startsWith('OKAY')) {
+                // OKAY = end of response for this command
+                returnData.text += response.substring(4);
+            } else if (response.startsWith('INFO')) {
+                // INFO = additional info line
+                returnData.text += response.substring(4) + '\n';
+            } else if (response.startsWith('DATA')) {
+                // DATA = hex string, but it's returned separately for safety
+                returnData.dataSize = response.substring(4);
+            } else {
+                // Assume FAIL or garbage data
+                throw new FastbootError(response.substring(0, 4), response.substring(4));
+            }
+        // INFO means that more packets are coming
+        } while (response.startsWith('INFO'));
+    
+        return returnData;
+    }
+
     async sendCommand(command) {
         // Command and response length is always 64 bytes regardless of protocol
         if (command.length > 64) {
@@ -93,27 +142,99 @@ export class FastbootDevice {
         await this.device.transferOut(0x01, cmdPacket);
         logDebug('command:', command);
 
-        // Construct response string for each message
-        let returnStr = ''
-        let response;
-        do {
-            let respPacket = await this.device.transferIn(0x01, 64);
-            response = new TextDecoder().decode(respPacket.data);
-            logDebug('response: packet', respPacket, 'string', response);
-    
-            if (response.startsWith('OKAY')) {
-                // OKAY = end of response for this command
-                returnStr += response.substring(4);
-            } else if (response.startsWith('INFO')) {
-                // INFO = additional info line
-                returnStr += response.substring(4) + '\n';
-            } else {
-                // Assume FAIL or garbage data
-                throw new FastbootError(response.substring(0, 4), response.substring(4));
+        return this.readResponse();
+    }
+
+    async getVariable(varName) {
+        let resp = (await this.sendCommand(`getvar:${varName}`)).text;
+        // Some bootloaders send whitespace around some variables
+        resp = resp.trim();
+        // According to the spec, non-existent variables should return empty
+        // responses, 
+        if (resp) {
+            return resp;
+        } else {
+            // Throw an error for compatibility reasons
+            throw new FastbootError('FAIL', 'No such variable (OKAY)');
+        }
+    }
+
+    // Maximum payload to download and flash
+    async getDownloadSize() {
+        try {
+            let resp = (await getVariable('max-download-size')).toLowerCase();
+            if (resp) {
+                // AOSP fastboot requires hex
+                return parseInt(resp, 16);
             }
-        // INFO means that more packets are coming
-        } while (response.startsWith('INFO'));
-    
-        return returnStr;
+        } catch (error) { /* Failed = no value, fallthrough */ }
+
+        // FAIL or empty variable means no max, set a reasonable limit to conserve memory
+        return DEFAULT_DOWNLOAD_SIZE;
+    }
+
+    async sendRawPayload(buffer) {
+        let i = 0;
+        let remainingBytes = buffer.byteLength;
+        while (remainingBytes > 0) {
+            let chunk = buffer.slice(i * this.maxPacketSize, (i + 1) * this.maxPacketSize);
+            if (i % 1000 == 0) {
+                logDebug(`  Sending ${chunk.byteLength} bytes to endpoint, ${remainingBytes} remaining, i=${i}`);
+            }
+            await this.device.transferOut(0x01, chunk);
+
+            remainingBytes -= chunk.byteLength;
+            i += 1;
+        }
+
+        logDebug(`Finished sending payload, ${remainingBytes} bytes remaining`);
+    }
+
+    async flashFile(partition, file) {
+        // Use current slot if partition is A/B
+        try {
+            if (await this.getVariable(`has-slot:${partition}`) == 'yes') {
+                partition += '_' + await this.getVariable('current-slot');
+            }
+        } catch (error) { /* Failed = not A/B, fallthrough */ }
+
+        let maxDlSize = await this.getDownloadSize();
+        let data = await readFileAsBuffer(file);
+        logDebug(`Flashing ${data.byteLength} bytes to ${partition}, ${maxDlSize} bytes per chunk`);
+
+        let totalXferd = 0;
+        while (totalXferd < data.byteLength) {
+            // Try to transfer as much as possible in this chunk
+            let xferSize = Math.min(data.byteLength - totalXferd, maxDlSize);
+            // Bootloader requires an 8-digit hex number
+            let xferHex = xferSize.toString(16).padStart(8, '0');
+            if (xferHex.length != 8) {
+                throw new FastbootError('FAIL', `Transfer size overflow: ${xferHex} is more than 8 digits`);
+            }
+
+            // Check with the device and make sure size matches
+            let downloadResp = await this.sendCommand(`download:${xferHex}`);
+            if (downloadResp.dataSize == null) {
+                throw new FastbootError('FAIL', `Unexpected response to download command: ${downloadResp.text}`);
+            }
+            xferSize = parseInt(downloadResp.dataSize, 16);
+            // Sanity check: we'll end up in an infinite loop if size is 0
+            if (xferSize == 0) {
+                throw new FastbootError('FAIL', `Bootloader returned download size 0: ${downloadResp.text}`);
+            }
+
+            logDebug(`Chunk: Flashing ${xferSize} bytes, ${data.byteLength - totalXferd} remaining, total sent: ${totalXferd} bytes`);
+            await this.sendRawPayload(data.slice(0, xferSize));
+            data = data.slice(xferSize);
+
+            logDebug('Chunk sent, waiting for response...');
+            await this.readResponse();
+
+            logDebug('Flashing chunk...');
+            await this.sendCommand(`flash:${partition}`);
+            totalXferd += xferSize;
+        }
+
+        logDebug(`Flashed ${partition} in chunks of ${maxDlSize} bytes, ${data.byteLength} bytes remaining`);
     }
 }
