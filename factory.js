28348456cc9fc1b35a6645d5d59982f88cd8c6e9
Danny Lin <danny@kdrag0n.dev>
2021-01-22 17:21:37 -0800

    factory: Remove unused import

--- factory.js
+++ factory.js
@@ -1,6 +1,5 @@
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT
 
-import { FastbootDevice } from "./fastboot.js";
 import * as common from "./common.js";
 
 const DB_VERSION = 1;
