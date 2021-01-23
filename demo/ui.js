053c9a196e3faa47c474ee9853cf2313b306a5cc
Danny Lin <danny@kdrag0n.dev>
2021-01-22 17:23:08 -0800

    Move simple UI to demo directory

--- demo/ui.js
+++ demo/ui.js
@@ -1,7 +1,7 @@
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT
 
-import { FastbootDevice } from "./fastboot.js";
-import * as Factory from "./factory.js";
+import { FastbootDevice } from "../fastboot.js";
+import * as Factory from "../factory.js";
 
 let device = new FastbootDevice();
 
