e7f1339fc8c64dd72411459e398a6b384a85638c
Arden Sinclair <general@ardensinclair.com>
2021-01-26 19:27:46 -0600

    Preparation for migration to Typescript

--- demo/ui.js
+++ demo/ui.js
@@ -1,6 +1,6 @@
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT
 
-import { FastbootDevice, FactoryImages } from "../dist/fastboot.min.js";
+import { FastbootDevice, FactoryImages } from "../lib/fastboot.min.js";
 
 let device = new FastbootDevice();
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,28 @@
-import { terser } from "rollup-plugin-terser";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
-    input: "lib/index.js",
-    output: [
-        {
-            file: "dist/fastboot.cjs",
-            format: "cjs",
-        },
-        {
-            file: "dist/fastboot.js",
-            format: "es",
-        },
-        {
-            file: "dist/fastboot.min.cjs",
-            format: "cjs",
-            plugins: [
-                terser(),
-            ],
-        },
-        {
-            file: "dist/fastboot.min.js",
-            format: "es",
-            plugins: [
-                terser(),
-            ],
-        },
-    ],
-    plugins: [
-        nodeResolve(),
-    ],
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'lib/fastboot.cjs',
+      format: 'cjs',
+    },
+    {
+      file: 'lib/fastboot.js',
+      format: 'es',
+    },
+    {
+      file: 'lib/fastboot.min.cjs',
+      format: 'cjs',
+      plugins: [terser()],
+    },
+    {
+      file: 'lib/fastboot.min.js',
+      format: 'es',
+      plugins: [terser()],
+    },
+  ],
+  plugins: [nodeResolve(), typescript({ module: 'ESNext' })],
 };
