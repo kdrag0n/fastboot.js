46045bedbb4fb89ef7d770e29e88de0d03ebc769
Danny Lin <danny@kdrag0n.dev>
2021-02-03 18:33:23 -0800

    Generate sourcemaps
    This makes it significantly easier to debug issues in production. We
    minify to reduce transfer size, not to hide the code -- it's open-source
    anyway.

--- rollup.config.js
+++ rollup.config.js
@@ -7,19 +7,23 @@ export default {
         {
             file: "dist/fastboot.cjs",
             format: "cjs",
+            sourcemap: true,
         },
         {
             file: "dist/fastboot.mjs",
             format: "es",
+            sourcemap: true,
         },
         {
             file: "dist/fastboot.min.cjs",
             format: "cjs",
+            sourcemap: true,
             plugins: [terser()],
         },
         {
             file: "dist/fastboot.min.mjs",
             format: "es",
+            sourcemap: true,
             plugins: [terser()],
         },
     ],
